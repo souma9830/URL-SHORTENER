e602cc139071edfb973341b62414a70856ec8238
souma9830 <prabirkumarshee8@gmail.com>
2025-11-18 09:57:30 +0530

    server done

--- public/script.js
+++ public/script.js
@@ -6,3 +6,21 @@ document.getElementById('shorten-form').addEventListener('submit',(event)=>{
     console.log(url);
     console.log(shortcode);   
 })
+try {
+    const response=await fetch("/shorten",{
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({url,shortcode})
+    });
+    if(response.ok){
+        alert("Form Submitted Succesfully");
+    }
+    else{
+      const errormsg=await response.text();
+      alert(errormsg)  
+    }
+
+} catch (error) {
+    console.log(error);
+    
+}
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const http = require("http");
 const fs = require("fs").promises;
 const path = require("path");
-
+const crypto=require("crypto");
 const PORT = 3000;
+const DATA_FILE=path.join("data","link.json")
+const loadlink= async()=>{
+    try {
+        const data= await fs.readFile(DATA_FILE,"utf-8");
+        return JSON.parse(data)
+    } catch (error) {
+        if(error.code === "ENOENT"){
+            await fs.writeFile(DATA_FILE, JSON.stringify({}));
+            return {};
+        }
+        throw error;
+    }
+}
+const saveLink=async(link)=>{
+    await fs.writeFile(DATA_FILE,JSON.stringify(link))
+}
 
 const server = http.createServer(async (req, res) => {
 
@@ -41,6 +57,32 @@ const server = http.createServer(async (req, res) => {
                 return res.end("404 Not Found");
             }
         }
+    }
+    if(req.method=== "POST" && req.url==="/shorten"){
+
+        const link= await loadlink();
+
+        let data="";
+        req.on("data",(chunk)=>{
+            data+=chunk;
+        })
+        req.on("end",async()=>{
+            console.log(data);
+            const {url, shortcode} = JSON.parse(data);
+            if(!url){
+                res.writeHead(400,{"Content-Type": "text/plain"});
+                return res.end("URL is Required");
+            }
+            const finalshortcode= shortcode || crypto.randomBytes(4).toString("hex");
+            if(link[finalshortcode]){
+                res.writeHead(400,{"Content-Type": "text/plain"});
+                return res.end("Short Code Already Exist Please Choose Another ");
+            }
+            link[finalshortcode]=url;
+            await saveLink(link);
+            res.writeHead(200,{"Content-Type":"application/json"});
+            res.end(JSON.stringify({success:true, shortcode: finalshortcode}))
+        })
     }
 });
 
