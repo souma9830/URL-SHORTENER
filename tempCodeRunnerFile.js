8e316f241cb7d7343e86147c9035c7ee6bbeadb8
souma9830 <prabirkumarshee8@gmail.com>
2025-11-19 18:21:51 +0530

    Done

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-       else{
-            const links=await loadlink();
-            const sortcode=req.url.slice(1);
-            if(links[sortcode]){
-                res.writeHead(302, { "Location": links[sortcode] });
-                return res.end();
-            }
-        }
